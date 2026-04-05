2f97be8e2faf03114dd8823b01e39c2714471f10
Anhgelus Morhtuuzh <william@herges.fr>
2026-04-05 15:54:22 +0200

    style(atproto): move lookupHandle in handle
    Align with DID's lookup

--- atproto/handle.go
+++ atproto/handle.go
@@ -84,6 +84,70 @@ func (h Handle) MarshalMap() (any, error) {
 	return h.String(), nil
 }
 
+func (h Handle) did(ctx context.Context, client *http.Client, resolver *net.Resolver) (*DID, error) {
+	ctx2, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	ch := make(chan *DID)
+
+	var err error
+	go func() {
+		res, err := resolver.LookupTXT(ctx, "_atproto."+h.String())
+		if err != nil {
+			ch <- nil
+			return
+		}
+		did, e := parseDidTxtRec(res)
+		if !errors.Is(e, ErrHandleNotFound) {
+			err = fmt.Errorf("cannot resolve via DNS records: %w", ErrCannotResolveHandle)
+		}
+		// avoid blocking goroutine
+		select {
+		case <-ctx2.Done():
+		default:
+			ch <- did
+		}
+	}()
+
+	select {
+	case <-ctx2.Done():
+	case did := <-ch:
+		if did != nil {
+			return did, nil
+		}
+	}
+
+	req, e := http.NewRequest(http.MethodGet, h.String()+"/.well-known/atproto-did", nil)
+	fn := func() error {
+		if e == nil {
+			return nil
+		}
+		if err != nil {
+			return errors.Join(err, fmt.Errorf("cannot resolve via HTTP: %w", e))
+		} else {
+			return e
+		}
+	}
+	resp, e := client.Do(req.WithContext(ctx))
+	if e != nil {
+		return nil, fn()
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 400 {
+		e = ErrHandleNotFound
+		return nil, fn()
+	}
+	b, e := io.ReadAll(resp.Body)
+	if e != nil {
+		return nil, fn()
+	}
+	did, e := ParseDID(strings.TrimSpace(string(b)))
+	if e == nil {
+		return did, nil
+	}
+	return nil, fmt.Errorf("%w: invalid DID in well-known: %w", ErrCannotResolveHandle, fn())
+}
+
 // Directory is used to get [DIDDocument] from [Handle] and [DID].
 //
 // We highly encourage you to implement your own [Directory] to limit requests with a cache.
@@ -122,19 +186,15 @@ func NewDirectory(client *http.Client, resolver *net.Resolver) *BaseDirectory {
 }
 
 func (d *BaseDirectory) ResolveDID(ctx context.Context, did *DID) (*DIDDocument, error) {
-	doc, err := did.document(ctx, d.client)
-	if err != nil {
-		return nil, err
-	}
-	return doc, nil
+	return did.document(ctx, d.client)
 }
 
 func (d *BaseDirectory) ResolveHandle(ctx context.Context, h Handle) (*DIDDocument, error) {
-	did, err := d.lookupHandle(ctx, h)
+	did, err := h.did(ctx, d.client, d.resolver)
 	if err != nil {
 		return nil, err
 	}
-	doc, err := did.document(ctx, d.client)
+	doc, err := d.ResolveDID(ctx, did)
 	if err != nil {
 		return nil, err
 	}
@@ -143,70 +203,6 @@ func (d *BaseDirectory) ResolveHandle(ctx context.Context, h Handle) (*DIDDocume
 		return nil, ErrInvalidHandle
 	}
 	return doc, nil
-}
-
-func (d *BaseDirectory) lookupHandle(ctx context.Context, h Handle) (*DID, error) {
-	ctx2, cancel := context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-
-	ch := make(chan *DID)
-
-	var err error
-	go func() {
-		res, err := d.resolver.LookupTXT(ctx, "_atproto."+h.String())
-		if err != nil {
-			ch <- nil
-			return
-		}
-		did, e := parseDidTxtRec(res)
-		if !errors.Is(e, ErrHandleNotFound) {
-			err = fmt.Errorf("cannot resolve via DNS records: %w", ErrCannotResolveHandle)
-		}
-		// avoid blocking goroutine
-		select {
-		case <-ctx2.Done():
-		default:
-			ch <- did
-		}
-	}()
-
-	select {
-	case <-ctx2.Done():
-	case did := <-ch:
-		if did != nil {
-			return did, nil
-		}
-	}
-
-	req, e := http.NewRequest(http.MethodGet, h.String()+"/.well-known/atproto-did", nil)
-	fn := func() error {
-		if e == nil {
-			return nil
-		}
-		if err != nil {
-			return errors.Join(err, fmt.Errorf("cannot resolve via HTTP: %w", e))
-		} else {
-			return e
-		}
-	}
-	resp, e := d.client.Do(req.WithContext(ctx))
-	if e != nil {
-		return nil, fn()
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode >= 400 {
-		e = ErrHandleNotFound
-		return nil, fn()
-	}
-	b, e := io.ReadAll(resp.Body)
-	if e != nil {
-		return nil, fn()
-	}
-	did, e := ParseDID(strings.TrimSpace(string(b)))
-	if e == nil {
-		return did, nil
-	}
-	return nil, fmt.Errorf("%w: invalid DID in well-known: %w", ErrCannotResolveHandle, fn())
 }
 
 func parseDidTxtRec(res []string) (*DID, error) {
