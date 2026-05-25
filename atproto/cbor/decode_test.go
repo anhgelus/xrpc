b9a0f2ae4576595f2b29b74f657a0e12724346de
Anhgelus Morhtuuzh <william@herges.fr>
2026-05-25 15:45:25 +0200

    refactor(cbor): simplify tests

--- atproto/cbor/decode_test.go
+++ atproto/cbor/decode_test.go
@@ -1,66 +1,63 @@
 package cbor
 
 import (
-	"slices"
+	"reflect"
 	"testing"
 )
 
-func doUnmarshal[T any](t *testing.T, v []byte, exp T, eq func(a, b T) bool) {
+func doUnmarshal[T any](t *testing.T, v []byte, exp T) {
 	var raw T
 	rest, err := Unmarshal(v, &raw)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if len(rest) != 0 {
-		t.Errorf("not everything was parsed: %v", rest)
+		t.Errorf("not everything was parsed: % x", rest)
 	}
-	if !eq(raw, exp) {
-		t.Errorf("bad result for %x: %v, wanted %v", v, raw, exp)
+	if !reflect.DeepEqual(raw, exp) {
+		t.Errorf("bad result for % x: %#v, wanted %#v", v, raw, exp)
 	}
 }
 
 func TestUnmarshal_Int(t *testing.T) {
-	eq1 := func(a, b uint) bool { return a == b }
-	doUnmarshal(t, []byte{0}, 0, eq1)
-	doUnmarshal(t, []byte{0x01}, 1, eq1)
-	doUnmarshal(t, []byte{0x0a}, 10, eq1)
-	doUnmarshal(t, []byte{0x17}, 23, eq1)
-	doUnmarshal(t, []byte{0x18, 0x18}, 24, eq1)
-	doUnmarshal(t, []byte{0x18, 0x64}, 100, eq1)
-	doUnmarshal(t, []byte{0x19, 0x03, 0xe8}, 1000, eq1)
-	doUnmarshal(t, []byte{0x1a, 0x00, 0x0f, 0x42, 0x40}, 1000000, eq1)
+	doUnmarshal(t, []byte{0}, 0)
+	doUnmarshal(t, []byte{0x01}, 1)
+	doUnmarshal(t, []byte{0x0a}, 10)
+	doUnmarshal(t, []byte{0x17}, 23)
+	doUnmarshal(t, []byte{0x18, 0x18}, 24)
+	doUnmarshal(t, []byte{0x18, 0x64}, 100)
+	doUnmarshal(t, []byte{0x19, 0x03, 0xe8}, 1000)
+	doUnmarshal(t, []byte{0x1a, 0x00, 0x0f, 0x42, 0x40}, 1000000)
 
-	eq2 := func(a, b int) bool { return a == b }
-	doUnmarshal(t, []byte{0x20}, -1, eq2)
-	doUnmarshal(t, []byte{0x21}, -2, eq2)
-	doUnmarshal(t, []byte{0x29}, -10, eq2)
-	doUnmarshal(t, []byte{0x38, 0x63}, -100, eq2)
-	doUnmarshal(t, []byte{0x39, 0x03, 0xe7}, -1000, eq2)
+	doUnmarshal(t, []byte{0x20}, -1)
+	doUnmarshal(t, []byte{0x21}, -2)
+	doUnmarshal(t, []byte{0x29}, -10)
+	doUnmarshal(t, []byte{0x38, 0x63}, -100)
+	doUnmarshal(t, []byte{0x39, 0x03, 0xe7}, -1000)
 }
 
 func TestUnmarshal_Bool(t *testing.T) {
-	doUnmarshal(t, []byte{0xf4}, false, func(a, b bool) bool { return a == b })
-	doUnmarshal(t, []byte{0xf5}, true, func(a, b bool) bool { return a == b })
-	var dummy *int
-	doUnmarshal(t, []byte{0xf6}, dummy, func(a, b *int) bool { return a == b })
+	doUnmarshal(t, []byte{0xf4}, false)
+	doUnmarshal(t, []byte{0xf5}, true)
+	var dummy *bool
+	doUnmarshal(t, []byte{0xf6}, dummy)
 }
 
 func TestUnmarshal_Bytes(t *testing.T) {
-	doUnmarshal(t, []byte{0x40}, []byte{}, slices.Equal)
-	doUnmarshal(t, []byte{0x43, 0x01, 0x02, 0x03}, []byte{0x01, 0x02, 0x03}, slices.Equal)
+	doUnmarshal(t, []byte{0x40}, []byte{})
+	doUnmarshal(t, []byte{0x43, 0x01, 0x02, 0x03}, []byte{0x01, 0x02, 0x03})
 }
 
 func TestUnmarshal_String(t *testing.T) {
-	eq := func(a, b string) bool { return a == b }
-	doUnmarshal(t, []byte{0x60}, "", eq)
-	doUnmarshal(t, []byte{0x61, 0x61}, "a", eq)
-	doUnmarshal(t, []byte{0x65, 0x68, 0x65, 0x6c, 0x6c, 0x6f}, "hello", eq)
-	doUnmarshal(t, []byte{0x64, 0x49, 0x45, 0x54, 0x46}, "IETF", eq)
-	doUnmarshal(t, []byte{0x62, 0xc3, 0xbc}, "ü", eq)
+	doUnmarshal(t, []byte{0x60}, "")
+	doUnmarshal(t, []byte{0x61, 0x61}, "a")
+	doUnmarshal(t, []byte{0x65, 0x68, 0x65, 0x6c, 0x6c, 0x6f}, "hello")
+	doUnmarshal(t, []byte{0x64, 0x49, 0x45, 0x54, 0x46}, "IETF")
+	doUnmarshal(t, []byte{0x62, 0xc3, 0xbc}, "ü")
 }
 
 func TestUnmarshal_Array(t *testing.T) {
-	doUnmarshal(t, []byte{0x80}, []byte{}, slices.Equal)
-	doUnmarshal(t, []byte{0x83, 0x01, 0x02, 0x03}, []int{1, 2, 3}, slices.Equal)
-	doUnmarshal(t, []byte{0x82, 0xf5, 0xf4}, []bool{true, false}, slices.Equal)
+	doUnmarshal(t, []byte{0x80}, []byte(nil))
+	doUnmarshal(t, []byte{0x83, 0x01, 0x02, 0x03}, []int{1, 2, 3})
+	doUnmarshal(t, []byte{0x82, 0xf5, 0xf4}, []bool{true, false})
 }
--- atproto/cbor/encode_test.go
+++ atproto/cbor/encode_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func doMarshal(t *testing.T, v any, exp []byte) {
+func doMarshal(t *testing.T, v any, exp ...byte) {
 	res, err := Marshal(v)
 	if err != nil {
 		t.Fatal(err)
@@ -16,60 +16,68 @@ func doMarshal(t *testing.T, v any, exp []byte) {
 }
 
 func TestMarshal_Int(t *testing.T) {
-	doMarshal(t, 0, []byte{0})
-	doMarshal(t, 1, []byte{1})
-	doMarshal(t, 10, []byte{0x0a})
-	doMarshal(t, 23, []byte{0x17})
-	doMarshal(t, 24, []byte{0x18, 0x18})
-	doMarshal(t, 100, []byte{0x18, 0x64})
-	doMarshal(t, 1000, []byte{0x19, 0x03, 0xe8})
-	doMarshal(t, 1000000, []byte{0x1a, 0x00, 0x0f, 0x42, 0x40})
+	doMarshal(t, 0, 0)
+	doMarshal(t, 1, 1)
+	doMarshal(t, 10, 0x0a)
+	doMarshal(t, 23, 0x17)
+	doMarshal(t, 24, 0x18, 0x18)
+	doMarshal(t, 100, 0x18, 0x64)
+	doMarshal(t, 1000, 0x19, 0x03, 0xe8)
+	doMarshal(t, 1000000, 0x1a, 0x00, 0x0f, 0x42, 0x40)
 
-	doMarshal(t, -1, []byte{0x20})
-	doMarshal(t, -2, []byte{0x21})
-	doMarshal(t, -10, []byte{0x29})
-	doMarshal(t, -100, []byte{0x38, 0x63})
-	doMarshal(t, -1000, []byte{0x39, 0x03, 0xe7})
+	doMarshal(t, -1, 0x20)
+	doMarshal(t, -2, 0x21)
+	doMarshal(t, -10, 0x29)
+	doMarshal(t, -100, 0x38, 0x63)
+	doMarshal(t, -1000, 0x39, 0x03, 0xe7)
 }
 
 func TestMarshal_Bool(t *testing.T) {
-	doMarshal(t, false, []byte{0xf4})
-	doMarshal(t, true, []byte{0xf5})
+	doMarshal(t, false, 0xf4)
+	doMarshal(t, true, 0xf5)
 	var dummy *int
-	doMarshal(t, dummy, []byte{0xf6})
+	doMarshal(t, dummy, 0xf6)
 }
 
 func TestMarshal_Bytes(t *testing.T) {
-	doMarshal(t, []byte(""), []byte{0x40})
-	doMarshal(t, []byte{0x01, 0x02, 0x03}, []byte{0x43, 0x01, 0x02, 0x03})
+	doMarshal(t, []byte(""), 0x40)
+	doMarshal(t, []byte{0x01, 0x02, 0x03}, 0x43, 0x01, 0x02, 0x03)
 }
 
 func TestMarshal_String(t *testing.T) {
-	doMarshal(t, "", []byte{0x60})
-	doMarshal(t, "a", []byte{0x61, 0x61})
-	doMarshal(t, "hello", []byte{0x65, 0x68, 0x65, 0x6c, 0x6c, 0x6f})
-	doMarshal(t, "IETF", []byte{0x64, 0x49, 0x45, 0x54, 0x46})
-	doMarshal(t, "ü", []byte{0x62, 0xc3, 0xbc})
+	doMarshal(t, "", 0x60)
+	doMarshal(t, "a", 0x61, 0x61)
+	doMarshal(t, "hello", 0x65, 0x68, 0x65, 0x6c, 0x6c, 0x6f)
+	doMarshal(t, "IETF", 0x64, 0x49, 0x45, 0x54, 0x46)
+	doMarshal(t, "ü", 0x62, 0xc3, 0xbc)
 }
 
 func TestMarshal_Array(t *testing.T) {
-	doMarshal(t, []bool{}, []byte{0x80})
-	doMarshal(t, []int{1, 2, 3}, []byte{0x83, 0x01, 0x02, 0x03})
-	doMarshal(t, []bool{true, false}, []byte{0x82, 0xf5, 0xf4})
+	doMarshal(t, []bool{}, 0x80)
+	doMarshal(t, []int{1, 2, 3}, 0x83, 0x01, 0x02, 0x03)
+	doMarshal(t, []bool{true, false}, 0x82, 0xf5, 0xf4)
 }
 
 func TestMarshal_Map(t *testing.T) {
-	doMarshal(t, map[string]any{}, []byte{0xa0})
-	doMarshal(t, map[string]int{"a": 1}, []byte{0xa1, 0x61, 0x61, 0x01})
-	doMarshal(t, map[string]int{"a": 1, "b": 2}, []byte{0xa2, 0x61, 0x61, 0x01, 0x61, 0x62, 0x02})
+	doMarshal(t, map[string]any{}, 0xa0)
+	doMarshal(t, map[string]int{"a": 1}, 0xa1, 0x61, 0x61, 0x01)
+	doMarshal(t, map[string]int{"a": 1, "b": 2}, 0xa2, 0x61, 0x61, 0x01, 0x61, 0x62, 0x02)
 }
 
-func doMarshalStruct(t *testing.T, v any, exp map[string]any) {
-	mp, err := Marshal(exp)
+func doMarshalStruct(t *testing.T, v any, exp ...any) {
+	if len(exp)%2 != 0 {
+		t.Fatal("invalid args")
+	}
+	ln := len(exp) / 2
+	cv := make(map[string]any, ln)
+	for i := range ln {
+		cv[exp[i*2].(string)] = exp[i*2+1]
+	}
+	mp, err := Marshal(cv)
 	if err != nil {
 		t.Fatal(err)
 	}
-	doMarshal(t, v, mp)
+	doMarshal(t, v, mp...)
 }
 
 func TestMarshal_Struct(t *testing.T) {
@@ -77,16 +85,16 @@ func TestMarshal_Struct(t *testing.T) {
 		A uint
 		B uint `json:"b"`
 		C uint `json:"d" cbor:"c"`
-	}{0, 1, 2}, map[string]any{"A": 0, "b": 1, "c": 2})
+	}{0, 1, 2}, "A", 0, "b", 1, "c", 2)
 	doMarshalStruct(t, struct {
 		A uint `cbor:",omitempty"`
 		B uint `json:"b,omitempty"`
 		C uint `cbor:"c,string"`
-	}{0, 0, 2}, map[string]any{"c": "2"})
+	}{0, 0, 2}, "c", "2")
 	doMarshalStruct(t, struct {
 		A uint `cbor:"a,omitempty,string"`
-	}{0}, map[string]any{})
+	}{0})
 	doMarshalStruct(t, struct {
 		A uint `cbor:"a,omitempty,string"`
-	}{1}, map[string]any{"a": "1"})
+	}{1}, "a", "1")
 }
